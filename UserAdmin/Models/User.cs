9a3dfb17e151fffd69356fdded0333bc2fa684cd
ipacslaci-83 <laciko83@gmail.com>
2026-09-16 10:37:27 +0200

    Register Page is modify

--- UserAdmin/Models/User.cs
+++ UserAdmin/Models/User.cs
@@ -8,7 +8,9 @@ namespace UserAdmin.Models
     {
         public int? Id { get; set; }
         public string? UserName { get; set; }
+        public string? Email { get; set; }
         public string? Password { get; set; }
         public DateTime RegisteredAt { get; set; }
+        
     }
 }
