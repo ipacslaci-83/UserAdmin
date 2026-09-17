a43a92a02bb888064c990a01485e61a817147c9d
ipacslaci-83 <laciko83@gmail.com>
2026-09-17 14:32:14 +0200

    LoginPage is created

---
+++ UserAdmin/Services/UserDbService.cs
@@ -0,0 +1,37 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using UserAdmin.Models;
+
+namespace UserAdmin.Services
+{
+    internal class UserDbService
+    {
+        public string ConnectionString = "Server=localhost;Database=useradmin;User=root;Password=;";
+        
+
+        public void Add (User user)
+        {
+            var connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+
+            string sql = @"INSERT INTO `users`(`username`,`email`,`password`,`registeredAt`) 
+                            VALUES (@Username,@Email,@Password,@RegisteredAt)";
+            
+            var cmd = new MySqlCommand(sql, connection);
+
+            cmd.Parameters.AddWithValue("@Username", user.UserName);
+            cmd.Parameters.AddWithValue("@Email", user.Email);
+            cmd.Parameters.AddWithValue("@Password", user.Password);
+            cmd.Parameters.AddWithValue("@RegisteredAt", user.RegisteredAt);
+            cmd.ExecuteNonQuery();
+     
+            connection.Close();
+            
+
+        }
+    }
+}
+
--- UserAdmin/Views/RegisterPage.xaml.cs
+++ UserAdmin/Views/RegisterPage.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserAdmin.Models;
+using UserAdmin.Services;
 
 namespace UserAdmin.Views
 {
@@ -18,9 +20,63 @@ namespace UserAdmin.Views
     /// </summary>
     public partial class RegisterPage : Page
     {
+        private readonly UserDbService _userdbservice = new UserDbService();
         public RegisterPage()
         {
             InitializeComponent();
+        }
+        private void Login_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            ErrorText.Visibility = Visibility.Collapsed;
+            ErrorText.Text = string.Empty;
+
+            var username = UsernameBox.Text.Trim();
+            var email = EmailBox.Text.Trim();
+            var password = PasswordBoxInput.Password;
+            var confirmpassword = ConfirmPasswordBox.Password;
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password) ||
+                string.IsNullOrWhiteSpace(confirmpassword))
+            {
+                ErrorText.Text = "Minden mező kitöltése kötelező";
+                ErrorText.Visibility = Visibility.Visible;
+                return;
+            }
+           
+
+            if (password.Length < 6)
+            {
+                ErrorText.Text = "Túl rövid a jelszó";
+                ErrorText.Visibility = Visibility.Visible;
+                return;
+            }
+            
+            
+            if (password != confirmpassword)
+            {
+                ErrorText.Text = "Nem egyezik a jelszó";
+                ErrorText.Visibility = Visibility.Visible;
+                return;
+            }
+            
+
+            
+            var user = new User
+            {
+                UserName = username,
+                Email = email,
+                Password = password,
+                RegisteredAt = DateTime.Now
+            };
+
+            _userdbservice.Add(user);
+
+            MessageBox.Show("Sikeres regisztráció!");
         }
     }
 }
